b1ff134dd71842b844cbfba2aecc7b6e48589375
Yiu Man Chi <ymcatar@gmail.com>
2015-09-01 22:06:25 +0800

    fixed not loading bug

--- assets/facebook.js
+++ assets/facebook.js
@@ -1,22 +1,8 @@
 require(["gitbook", "jquery"], function(gitbook, $) {
 
-    var started = false;
-    var appKey;
-
     var init = function() {
-
-        if (!started) return;
-
-        $('body').prepend('<div id="fb-root"></div><script>(function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4&appId=' + appKey + '"; fjs.parentNode.insertBefore(js, fjs);}(document, "script", "facebook-jssdk"));</script>');
-
-        $('.page-inner').append('<br><div class="fb-comments" data-href="' + window.location.href + '" data-numposts="5"></div>');
+        FB.XFBML.parse();
     };
-
-    gitbook.events.bind('start', function(e, config) {
-        started = true;
-        appKey = config.facebook.appKey;
-        init();
-    });
 
     gitbook.events.bind("page.change", init);
 
--- index.js
+++ index.js
@@ -7,6 +7,19 @@ module.exports = {
         ],
         js: [
             "facebook.js"
-        ]
+        ],
+        html: {
+            "body:start": function() {
+                var appKey = this.options.pluginsConfig.facebook.appKey;
+                return '<div id="fb-root"></div><script>(function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4&appId=' + appKey + '"; fjs.parentNode.insertBefore(js, fjs);}(document, "script", "facebook-jssdk"));</script>';
+            }
+        }
+    },
+
+    hooks: {
+        "page": function(page) {
+            page.sections[0].content += '<br><div class="fb-comments" data-numposts="5"></div>';
+            return page;
+        }
     }
 };
