9585f78f305827be4e011a7a4cae2afa7e320da7
Yiu Man Chi <ymcatar@gmail.com>
2015-09-03 00:22:57 +0800

    facebook sdk loaded synchronously

--- assets/facebook.js
+++ assets/facebook.js
@@ -1,11 +1,14 @@
 require(["gitbook", "jquery"], function(gitbook, $) {
 
     var init = function() {
+        FB.init({
+            appId: $('.fb_id').data('id'),
+            xfbml: true
+        });
+
         $('fb-comments').data('href', window.location);
         FB.XFBML.parse();
     };
 
-    gitbook.events.bind("start", init);
     gitbook.events.bind("page.change", init);
-
 });
--- index.js
+++ index.js
@@ -6,12 +6,13 @@ module.exports = {
             "facebook.css"
         ],
         js: [
+            "http://connect.facebook.net/en_US/all.js",
             "facebook.js"
         ],
         html: {
             "body:start": function() {
                 var appKey = this.options.pluginsConfig.facebook.appKey;
-                return '<script>(function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4&appId=' + appKey + '"; fjs.parentNode.insertBefore(js, fjs);}(document, "script", "facebook-jssdk"));</script>';
+                return '<div class="fb_id" data-id="' + appKey +'"></div>';
             }
         }
     },
